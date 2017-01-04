40e9dd8449fbd147edf2f99ca29a9eec01a200f0
guan12 <1109553680@qq.com>
2017-01-04 22:21:27 +0800

    视频部分 (#10)

---
+++ js/video.js
@@ -0,0 +1,14 @@
+$(function(){
+	var imgArr = [
+		"img/video_img/L_1.png",
+		"img/video_img/L_2.png",
+		"img/video_img/L_2.png",
+		"img/video_img/L_1.png"
+	]
+	var img = $(".slide-pan img");
+	img.map(function(index,item){
+		$(item).attr("src",imgArr[index]);
+	});
+	var tag = $("#footer li");
+	tag.eq(1).addClass("page-active");
+})
