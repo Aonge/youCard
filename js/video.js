0e004027d1fe3750d3be168b1d8e21529e8fc9f1
binbin <lanouhn@binbin.local>
2017-01-05 10:42:46 +0800

    修改部分页面bug，调整部分样式，加入二级页面头部

--- js/video.js
+++ js/video.js
@@ -9,6 +9,6 @@ $(function(){
 	img.map(function(index,item){
 		$(item).attr("src",imgArr[index]);
 	});
-	var tag = $("#footer li");
+	var tag = $("#footer li a");
 	tag.eq(1).addClass("page-active");
 })
