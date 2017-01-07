8608f13a6834b33bb6848b9c03adcf5c306415f1
lanouhn <2296896041@qq.com>
2017-01-07 22:22:09 +0800

    关注和收藏点击事件

--- js/attention.js
+++ js/attention.js
@@ -1,4 +1,18 @@
-$(function(){
+$(function() {
 	var tag = $("#footer li");
 	tag.eq(0).addClass("page-active");
+})
+var btn = document.getElementsByTagName("button");
+$(function() {
+	touch.on(btn, "tap", function() {
+		if(this.innerHTML == "取消关注") {
+			this.style.color = "white";
+			this.style.background = "#f56243";
+			this.innerHTML = "关注";
+		}else{
+			this.style.color = "#f56243";
+			this.style.background = "white";
+			this.innerHTML = "取消关注";
+		}
+	})
 })
