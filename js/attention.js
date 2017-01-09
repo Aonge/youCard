f61f1097358230910086b804a59beb37b8831b3a
binbin <lanouhn@binbin.local>
2017-01-09 21:05:35 +0800

    评分bug解决

--- js/attention.js
+++ js/attention.js
@@ -1,35 +1,39 @@
-$(function(){
+$(function() {
 	var tag = $("#footer li");
 	tag.eq(0).addClass("page-active");
 	var care = $(".w-attentionList button");
 	var star = $(".favorite");
-//	star.map(function(index,item){
-////		console.log(item)
-//		item.firm = false;
-//		item.index = index;
-//	})
-	touch.on(care,"tap",function(){
-		if ($(this).html() === "取消关注") {
+	//	star.map(function(index,item){
+	////		console.log(item)
+	//		item.firm = false;
+	//		item.index = index;
+	//	})
+	touch.on(care, "tap", function() {
+		if($(this).html() === "取消关注") {
 			$(this).html("关注");
-		}else{
+		} else {
 			$(this).html("取消关注")
 		}
 		var parent = this.parentNode;
 	});
-	touch.on(star,"swipestart swiping swipeend",function(event){	
+	touch.on(star, "swipestart swiping swipeend", function(event) {
 		event.preventDefault();
-		if (!$(this).parent().firm) {
-			var fillStart = $(this).parent().next()
-			fillStart.width(event.distanceX);
-		}else{
-			return;
-		}
-		if (event.type == "swipeend") {
-			if(confirm("确定提交评分?")){
-				$(this).parent().firm = true;
-			}else{
+		if($(this).prev().html() != "游戏评级：") {
+			if($(this).parent().attr("firm") =="true") {
+				var fillStart = $(this).parent().next()
+				fillStart.width(event.distanceX);
+			} else {
 				return;
 			}
+			if(event.type == "swipeend") {
+				if(confirm("确定提交评分?")) {
+					$(this).parent().attr("firm","false");
+				} else {
+					return;
+				}
+			}
+		}else{
+			return;
 		}
 	});
 })
